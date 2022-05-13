792987278f393f99853f956694ec96c743ac808e
brunozn <brunojndias@gmail.com>
2022-05-13 17:12:40 -0300

    configurações para usar o emulador do cloud firestore

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import 'react-native-gesture-handler';
+
+import '@react-native-firebase/app';
+import firestore from '@react-native-firebase/firestore';
 import { registerRootComponent } from 'expo';
 
+
 import App from './App';
+import { firebase } from '@react-native-firebase/firestore';
+
+if (__DEV__){
+  firestore().useEmulator('192.168.0.22', 8080);
+}
+
+const db = firestore();
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
