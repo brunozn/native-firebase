197be4036a6d513cf5ff229512ab9bbc2d8fad8f
brunozn <brunojndias@gmail.com>
2022-05-13 15:23:51 -0300

    ordenando consultas com o orderBy

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -13,7 +13,7 @@ export function ShoppingList() {
   useEffect( () => {
     const subscribe = firestore()
     .collection('products')
-    .limit(3)
+    .orderBy('description', 'desc')
     .onSnapshot(querySnapshot => {
       const data = querySnapshot.docs.map( (doc) => {
         return {
